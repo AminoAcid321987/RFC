5b334783c06f6c74f6a93f0f86685e401bfa4e5e
AminoAcid321987 <asdfyes135@gmail.com>
2025-09-07 16:00:09 +0800

    Add files via upload

--- nlp.py
+++ nlp.py
@@ -9,8 +9,36 @@ import re
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
-Data_Frame = pd.read_csv("train.txt",delimiter=";", names=["Statement","Emotion"])
+Data_Frame = pd.read_csv("train.txt",delimiter=";", names=["Statement","Emotion"]).head(20)
 
 Var = "Please just write an actual sentence, that's all you have to do."
 Var2 = re.sub("[^a-zA-Z]"," ",Var).lower()
-print(Var2)
+
+MyList = []
+for i in Var2.split():
+    if i not in stopwords.words("english"):
+        MyList.append(i)
+
+NewList = ["running", "cried", "crying", "cries"]
+for i in NewList:
+    A = WordNetLemmatizer().lemmatize(i)
+
+def Statements(Data_Frame):
+    Column_One = Data_Frame["Statement"]
+    EmptyList = []
+    WordList = []
+    for i in Data_Frame["Statement"]:
+        New_Sentence = re.sub("[^a-zA-Z]"," ",i).lower()
+        for i in New_Sentence.split():
+            if i not in stopwords.words("english"):
+                K = WordNetLemmatizer().lemmatize(i)
+                WordList.append(K)
+                Joined = " ".join(WordList)
+    print(Joined)
+
+Statements(Data_Frame)
+
+
+
+
+    
