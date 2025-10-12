53f91db52fc054b06935f4d89b888a11ad048672
AminoAcid321987 <asdfyes135@gmail.com>
2025-10-12 15:59:06 +0800

    Add files via upload

--- nlp.py
+++ nlp.py
@@ -2,6 +2,10 @@ import numpy as np
 
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import accuracy_score as ac
+
 import nltk
 nltk.download("stopwords")
 nltk.download("wordnet")
@@ -9,7 +13,7 @@ import re
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
-Data_Frame = pd.read_csv("train.txt",delimiter=";", names=["Statement","Emotion"]).head(20)
+Data_Frame = pd.read_csv("train.txt",delimiter=";", names=["Statement","Emotion"])
 
 Var = "Please just write an actual sentence, that's all you have to do."
 Var2 = re.sub("[^a-zA-Z]"," ",Var).lower()
@@ -26,8 +30,8 @@ for i in NewList:
 def Statements(Data_Frame):
     Column_One = Data_Frame
     EmptyList = []
-    WordList = []
     for i in Data_Frame:
+        WordList = []
         New_Sentence = re.sub("[^a-zA-Z]"," ",i).lower()
         for j in New_Sentence.split():
             if j not in stopwords.words("english"):
@@ -46,14 +50,18 @@ objct = CV(ngram_range=(1,2))
 x = objct.fit_transform(AllStatements)
 y = Data_Frame["Emotion"]
 
+x_train,x_test,y_train,y_test = train_test_split(x,y,train_size=0.8)
+
 from sklearn.ensemble import RandomForestClassifier 
 
 RFC = RandomForestClassifier(n_estimators=100)
 
-RFC.fit(x,y)
+RFC.fit(x_train,y_train)
 
-Sample_Sentence = Statements(["I am sad."])
-objct2 = CV.transform(Sample_Sentence)
+Sample_Sentence = Statements(["Today, I am currently furious, this is because I got my exam results back and they were bad."])
+objct2 = objct.transform(Sample_Sentence)
 
-objct3 = RFC.predict(objct2)
+objct3 = RFC.predict(x_test)
 print(objct3)
+
+ac(objct3,y_test)
